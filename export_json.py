724600b168e95ba30c529e870db40c9c26f3ee37
jjyoung-2005 <joshjyoung27@gmail.com>
2026-05-19 21:17:57 -0500

    Repair: json fixed

--- export_json.py
+++ export_json.py
@@ -7,11 +7,14 @@ import sqlite3
 conn = sqlite3.connect('emotions.db')
 df = pd.read_sql("SELECT * FROM scored_posts", conn)
 
-emotion_dict = {}
-
 avg_by_emotion = df.groupby('emotion')['compound'].mean().to_dict()
 
 count_for_sentiment = df['sentiment'].value_counts().to_dict()
+
+emotion_dict = {
+    "avg_by_emotion": avg_by_emotion,
+    "sentiment_counts": count_for_sentiment
+}
 
 with open('summary.json', 'w') as f:
     json.dump(emotion_dict, f)
